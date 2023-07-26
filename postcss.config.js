c8eb404fe65ec01969a7f48c315885df01b10fda
Xin <xin@imfing.com>
2023-07-27 00:05:47 +0100

    feat: implement common components
    feat: implement sidebar
    
    feat: implement footer
    
    feat: implement breadcrumb
    
    chore: add typography styles
    
    chore: add heading anchor link
    
    chore: add postcss-nested

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 let tailwindConfig = process.env.HUGO_FILE_TAILWIND_CONFIG_JS || './tailwind.config.js';
 const tailwind = require('tailwindcss')(tailwindConfig);
 const autoprefixer = require('autoprefixer');
+const nested = require('postcss-nested')
 
 module.exports = {
-	// eslint-disable-next-line no-process-env
-	plugins: [tailwind, ...(process.env.HUGO_ENVIRONMENT === 'production' ? [autoprefixer] : [])],
+	plugins: [tailwind, nested, ...(process.env.HUGO_ENVIRONMENT === 'production' ? [autoprefixer] : [])], // add nesting to the plugins array
 };
