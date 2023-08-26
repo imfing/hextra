af77173a3575250e02a72fda1d4668d75920616f
Xin <xin@imfing.com>
2023-08-26 12:21:02 +0100

    chore: update build configs

--- postcss.config.js
+++ postcss.config.js
@@ -7,6 +7,6 @@ module.exports = {
 		require('postcss-import'),
 		require('@tailwindcss/nesting'),
 		tailwind,
-		...(process.env.HUGO_ENVIRONMENT === 'production' ? [autoprefixer] : [])
+		autoprefixer
 	],
 };
