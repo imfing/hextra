de9389cd8dc792bf92bef9287913786a2ae8f817
Xin <xin@imfing.com>
2023-10-20 20:55:44 +0100

    feat: respect site theme default setting

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,40 +1,41 @@
-// Dark theme toggle
+// Light / Dark theme toggle
+(function () {
+  const defaultTheme = '{{ site.Params.theme.default | default `system`}}'
 
 const themeToggleButtons = document.querySelectorAll(".theme-toggle");
 
-// Change the icons inside the button based on previous settings
+  // Change the icons of the buttons based on previous settings or system theme
 if (
   localStorage.getItem("color-theme") === "dark" ||
-  (!("color-theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
+    (!("color-theme" in localStorage) &&
+      ((window.matchMedia("(prefers-color-scheme: dark)").matches && defaultTheme === "system") || defaultTheme === "dark"))
 ) {
   themeToggleButtons.forEach((el) => el.dataset.theme = "dark");
 } else {
   themeToggleButtons.forEach((el) => el.dataset.theme = "light");
 }
 
+  // Add click event handler to the buttons
 themeToggleButtons.forEach((el) => {
   el.addEventListener("click", function () {
     if (localStorage.getItem("color-theme")) {
       if (localStorage.getItem("color-theme") === "light") {
-        document.documentElement.classList.add("dark");
-        document.documentElement.style.colorScheme = "dark";
+          setDarkTheme();
         localStorage.setItem("color-theme", "dark");
       } else {
-        document.documentElement.classList.remove("dark");
-        document.documentElement.style.colorScheme = "light";
+          setLightTheme();
         localStorage.setItem("color-theme", "light");
       }
     } else {
       if (document.documentElement.classList.contains("dark")) {
-        document.documentElement.classList.remove("dark");
-        document.documentElement.style.colorScheme = "light";
+          setLightTheme();
         localStorage.setItem("color-theme", "light");
       } else {
-        document.documentElement.classList.add("dark");
-        document.documentElement.style.colorScheme = "dark";
+          setDarkTheme();
         localStorage.setItem("color-theme", "dark");
       }
     }
     el.dataset.theme = document.documentElement.classList.contains("dark") ? "dark" : "light";
   });
 });
+})();
