68e1e251191b9c8e167a1e3a928a8caa4f82ab7a
Xin <xin@imfing.com>
2024-04-29 22:36:48 +0100

    feat: caching for sidebar items from data

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,3 +1,12 @@
+/**
+ * Check if the element is visible.
+ * @param {Element} element Dom element
+ * @returns boolean
+ */
+function isVisible(element) {
+  return element.offsetWidth > 0 || element.offsetHeight > 0;
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   const buttons = document.querySelectorAll(".hextra-sidebar-collapsible-button");
   buttons.forEach(function (button) {
@@ -5,8 +14,41 @@ document.addEventListener("DOMContentLoaded", function () {
       e.preventDefault();
       const list = button.parentElement.parentElement;
       if (list) {
-        list.classList.toggle("open")
+        list.classList.toggle("open");
       }
     });
   });
+
+  const isCached = "{{- site.Params.page.sidebar.cache | default false -}}" === "true";
+  const currentPagePath = window.location.href;
+
+  if (isCached) {
+    // find the current page in the sidebar and open the parent lists
+    const sidebar = document.querySelector(".hextra-sidebar-container");
+    if (sidebar) {
+      // find a tags and compare href with current page path
+      const links = sidebar.querySelectorAll("a");
+      links.forEach(function (link) {
+        const linkPath = link.href;
+
+        if (currentPagePath === linkPath) {
+          // add active class to the link
+          link.classList.add("sidebar-item-active");
+          link.classList.remove("sidebar-item-inactive");
+
+          if (!isVisible(link)) {
+            return;
+          }
+          // recursively open parent lists
+          let parent = link.parentElement;
+          while (parent && !parent.classList.contains("hextra-sidebar-container")) {
+            if (parent.tagName === "LI" && parent.classList.contains("sidebar-item-list")) {
+              parent.classList.add("open");
+            }
+            parent = parent.parentElement;
+          }
+        }
+      });
+    }
+  }
 });
