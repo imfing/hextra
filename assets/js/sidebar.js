96aca6ed3ea9aa1e3077513b4e782661bec3c0c9
XUE <46087743+xue68@users.noreply.github.com>
2024-11-03 19:38:46 +0800

    Update sidebar.js

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
+  restoreSidebarPosition();
+  enableCollapsibles();
+});
+
+function enableCollapsibles() {
   const buttons = document.querySelectorAll(".hextra-sidebar-collapsible-button");
   buttons.forEach(function (button) {
     button.addEventListener("click", function (e) {
@@ -9,4 +14,33 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   });
-});
+}
+
+function saveSidebarPosition() {
+  const sidebarScrollbar = document.querySelector("aside.sidebar-container > .hextra-scrollbar");
+  if (sidebarScrollbar) {
+    const scrollPosition = sidebarScrollbar.scrollTop;
+    sessionStorage.setItem('sidebarScrollPosition', scrollPosition);
+  }
+}
+
+function restoreSidebarPosition() {
+  const sidebarScrollbar = document.querySelector("aside.sidebar-container > .hextra-scrollbar");
+  if (sidebarScrollbar) {
+    // get savedPosition
+    const savedPosition = sessionStorage.getItem('sidebarScrollPosition');
+    
+    if (savedPosition !== null) {
+      // restore the save SidebarPosition
+      sidebarScrollbar.scrollTo({
+        top: parseInt(savedPosition),
+        behavior: 'instant'
+      });
+    }
+
+    // save new position
+    sidebarScrollbar.addEventListener('scroll', function() {
+      saveSidebarPosition();
+    });
+  }
+}
