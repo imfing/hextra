0bf247586ae8faa0927b6b584e876e5a1ad42052
Fernandez Ludovic <ldez@users.noreply.github.com>
2025-08-23 18:47:59 +0200

    fix: banner with the burger navbar

--- assets/js/banner.js
+++ assets/js/banner.js
@@ -1,9 +1,10 @@
 // {{- if site.Params.banner }}
 (function () {
-  const closeBtn = document.querySelector(".hextra-top-banner .hextra-close-banner");
+  const closeBtn = document.querySelector(".hextra-banner .hextra-banner-close-button");
 
   closeBtn.addEventListener("click", () => {
-    document.documentElement.classList.add("hextra-top-banner-hidden");
+    document.documentElement.classList.add("hextra-banner-hidden");
+    document.documentElement.style.setProperty("--hextra-banner-height", "0px");
 
     localStorage.setItem('{{ site.Params.banner.key | default `banner-closed` }}', "0");
   });
